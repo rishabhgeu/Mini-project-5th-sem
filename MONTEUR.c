e07b61565b26d0bad9bd524f9a22d8937951d004
Rishabh rastogi <rishabhrastogi03@gmail.com>
2015-11-18 12:40:12 +0530

    Update MONTEUR.c

--- MONTEUR.c
+++ MONTEUR.c
@@ -11,13 +11,13 @@ Note: Input file has to be in the same format as INPUT.txt .
 
 void main()
 {
-  char a[10],ad[10],label[10],opcode[10],operand[10],symbol[10],ch,fnm[15],ab[15];
-  int st,diff,i,address,address1,add,len,alen,finaddr,prevaddr,j=0;
-  char mnemonic[15][15]={"LDA","STA","LDCH","STCH","ADD","SUB","MUL","DIV"};
+ char a[10],ad[10],label[10],opcode[10],operand[10],symbol[10],ch,fnm[15],ab[15];
+  int cT=0,st,diff,i,address,address1,add,len,alen,finaddr,prevaddr,j=0;
+    char mnemonic[15][15]={"LDA","STA","LDCH","STCH"};
   int locctr=0x0,start,l,c=0x0,k=0,bol=0;
-  char code[15][15]={"00","0C","50","54","28","29","3A","3B",};
+  char code[15][15]={"00","0C","50","54"};
   FILE *fp1,*fp2,*fp3,*fp4;
-printf("ENTER THE PROGRAM FILE NAME  : \n");
+  printf("ENTER THE PROGRAM FILE NAME  : \n");
 scanf("%s",&fnm);
   fp1 = fopen("MEDIATOR.txt","w");
   fp3 = fopen(fnm,"r");
@@ -130,7 +130,7 @@ printf("\n\nSTART OF PASS 2 ");
    fprintf(fp4,"H^%s^00%s^00%X\n",label,operand,finaddr);
    fscanf(fp3,"%X%s%s%s",&address,label,opcode,operand);
    st=address;
-   diff=prevaddr-st;
+   diff=finaddr-st;
     fprintf(fp4,"T^00%X^%X",address,diff);
   }
   while(strcmp(opcode,"END")!=0)
